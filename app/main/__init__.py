72957118314717f3c8b1c1072ef87d16de1afe5a
ldanielx <lucasdanielbarradas@gmail.com>
2022-09-05 22:06:45 -0300

    Add flask_cors in project

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 
 from .config import config_by_name
 from flask.app import Flask
@@ -11,6 +12,8 @@ flask_bcrypt = Bcrypt()
 
 def create_app(config_name: str) -> Flask:
     app = Flask(__name__)
+    CORS(app)
+
     app.config.from_object(config_by_name[config_name])
     db.init_app(app)
     flask_bcrypt.init_app(app)
