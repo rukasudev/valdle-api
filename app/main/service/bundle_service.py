a317130b635f64ace783efbb2eeaafa275c2d3a8
Lucas Daniel <lucasdanielbarradas@gmail.com>
2023-07-05 20:41:49 -0300

    Improve bundle service logic

--- app/main/service/bundle_service.py
+++ app/main/service/bundle_service.py
@@ -1,16 +1,12 @@
-from ..config import Config
+from util import get_by_valorant_api
 
-import requests
 import random
 
 
-def get_all_bundles(language: str) -> list:
+def _get_valorant_bundles(language: str) -> list:
     """Get all bundles from Valorant API"""
 
-    query_param = f"?language={language}" if language else ""
-    base_url = Config.VALORANT_API + f"/bundles{query_param}"
-    request = requests.get(base_url).json()
-    response = list(request["data"])
+    response = get_by_valorant_api("bundles", language)
 
     # remove bundles without promo_image
     response.pop(36)
@@ -19,37 +15,15 @@ def get_all_bundles(language: str) -> list:
     return response
 
 
-def get_bundle_by_index(index: int) -> dict:
-    """(Source: Bundles from Valorant API) Get bundle by index"""
-
-    request = get_all_bundles()
-    response = dict(request[index])
-    return response
-
-
 def get_random_bundle_with_image(language: str) -> dict:
     """Get a random bundle with image and choices"""
 
-    bundles = get_all_bundles(language)
-    choices = []
-    choices_index = []
+    bundles = _get_valorant_bundles(language)
+    choices = random.sample(bundles, 4)
+    answer_bundle = random.choice(choices)
 
-    while len(choices) < 4:
-        sorted_index = random.randint(0, len(bundles) - 1)
-        if sorted_index in choices_index:
-            continue
-
-        random_bundle = bundles[sorted_index]
-        display_name = random_bundle["displayName"]
-        choices_index.append(sorted_index)
-        choices.append(display_name)
-
-    answer_index = random.choice(choices_index)
-    answer_bundle = bundles[answer_index]
-    response = dict(
-        bundle_image=answer_bundle["verticalPromoImage"],
-        answer=answer_bundle["displayName"],
-        choices=choices,
-    )
-
-    return response, 200
+    return {
+        "bundle_image": answer_bundle["verticalPromoImage"],
+        "answer": answer_bundle["displayName"],
+        "choices": [bundle["displayName"] for bundle in choices],
+    }
---
+++ app/main/service/util.py
@@ -0,0 +1,12 @@
+from ..config import Config
+
+import requests
+
+def get_by_valorant_api(path: str, language: str) -> str:
+    query_param = f"?language={language}" if language else ""
+    base_url = f"{Config.VALORANT_API}/{path}{query_param}"
+    
+    request = requests.get(base_url).json()
+    response = list(set(request["data"]))
+    
+    return response
